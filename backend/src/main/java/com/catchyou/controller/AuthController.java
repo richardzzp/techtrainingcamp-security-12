49e2edfeebbcf5ca88cc28b54fe7c4ede54f6062
ZhouYiJoe <1287444832@qq.com>
2021-10-30 13:10:31 +0800

    把AuthController的接口路径改为"/auth"

--- backend/src/main/java/com/catchyou/controller/AuthController.java
+++ backend/src/main/java/com/catchyou/controller/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/auth")
 public class AuthController {
     /**
      * 获取验证码
