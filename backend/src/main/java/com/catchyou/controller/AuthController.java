a413ea25e24616830a04bc23d5d0f55f9c6df08e
ZhouYiJoe <1287444832@qq.com>
2021-10-30 12:38:33 +0800

    把控制器的名称改为"AuthController"

--- backend/src/main/java/com/catchyou/controller/AuthController.java
+++ backend/src/main/java/com/catchyou/controller/AuthController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/ljh")
-public class LjhAuthController {
+public class AuthController {
     /**
      * 获取验证码
      * @param requestBody
