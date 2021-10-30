d89499f51caa0c232b0f916dcae8a43bcd74d5c9
ZhouYiJoe <1287444832@qq.com>
2021-10-30 13:14:21 +0800

    修改接口的注释

--- backend/src/main/java/com/catchyou/controller/AuthController.java
+++ backend/src/main/java/com/catchyou/controller/AuthController.java
@@ -14,18 +14,18 @@ public class AuthController {
     /**
      * 获取验证码
      * @param requestBody
-     * PhoneNumber    //手机号
-     * Environment{
-     *     IP         //IP
-     *     DeviceID   //设备ID
+     * phoneNumber    //手机号
+     * environment{
+     *     ip         //IP
+     *     deviceId   //设备ID
      * }
      * @return
-     * Code     //0表示请求成功，1表示请求失败
-     * Message  //表示返回的说明，例如code=1时，message=“请求成功”
-     * Data{
-     *     VerifyCode     //6位随机的数字
-     *     ExpireTime     //验证码过期的时间，例如有效期3分钟，这个时间可以自行设定
-     *     DecisionType   //0表示正常下发验证码，1表示需要用户通过滑块验证，通过后才能请求验证码，2表示需要用户过一段时间，才能重试获取验证码，3表示对这个用户不下发验证码
+     * code     //0表示请求成功，1表示请求失败
+     * message  //表示返回的说明，例如code=1时，message=“请求成功”
+     * data{
+     *     verifyCode     //6位随机的数字
+     *     expireTime     //验证码过期的时间，例如有效期3分钟，这个时间可以自行设定
+     *     decisionType   //0表示正常下发验证码，1表示需要用户通过滑块验证，通过后才能请求验证码，2表示需要用户过一段时间，才能重试获取验证码，3表示对这个用户不下发验证码
      * }
      */
     @PostMapping("/apply-code")
@@ -37,26 +37,26 @@ public class AuthController {
      * 登录
      * @param requestBody
      * - 根据用户名登录的请求参数：
-     * UserName
-     * Password
-     * Environment{
-     *     IP
-     *     DeviceID
+     * userName
+     * password
+     * environment{
+     *     ip
+     *     deviceId
      * }
      * - 根据手机号登录的请求参数：
-     * PhoneNumber
-     * VerifyCode
-     * Environment{
-     *     IP
-     *     DeviceID
+     * phoneNumber
+     * verifyCode
+     * environment{
+     *     ip
+     *     deviceId
      * }
      * @return
-     * Code      //0表示登录成功，1表示登录失败
-     * Message   //表示返回的说明，例如code=1时，message=“用户名或者密码不对”
-     * Data{
-     *     SessionID
-     *     ExpireTime
-     *     DecisionType //0表示用户可以正常登录，1表示需要用户通过滑块验证，通过后才能登录，2表示需要用户过一段时间，才能重新登录，3表示这个用户不能登录
+     * code      //0表示登录成功，1表示登录失败
+     * message   //表示返回的说明，例如code=1时，message=“用户名或者密码不对”
+     * data{
+     *     sessionId
+     *     expireTime
+     *     decisionType //0表示用户可以正常登录，1表示需要用户通过滑块验证，通过后才能登录，2表示需要用户过一段时间，才能重新登录，3表示这个用户不能登录
      * }
      */
     @PostMapping("/login")
@@ -67,22 +67,22 @@ public class AuthController {
     /**
      * 注册
      * @param requestBody
-     * UserName     //用户名
-     * Password     //密码
-     * PhoneNumber
-     * VerifyCode
-     * Environment{
-     *     IP
-     *     DeviceID
+     * userName     //用户名
+     * password     //密码
+     * phoneNumber
+     * verifyCode
+     * environment{
+     *     ip
+     *     deviceId
      * }
      * @return
-     * Code      //0表示注册成功，1表示注册失败
-     * Message   //表示返回的说明，例如code=1时，message=“相同的用户名已经被注册过了，请更换用户名试试”
-     * SessionID //uuid
-     * Data{
-     *     SessionID    //随机的uuid
-     *     ExpireTime   //过期时间，例如有效期3小时，这个时间可以自行设定
-     *     DecisionType //0表示用户可以正常注册，1表示需要用户通过滑块验证，通过后才能注册，2表示需要用户过一段时间，才能重新注册，3表示这个用户不能注册
+     * code      //0表示注册成功，1表示注册失败
+     * message   //表示返回的说明，例如code=1时，message=“相同的用户名已经被注册过了，请更换用户名试试”
+     * sessionId //uuid
+     * data{
+     *     sessionId    //随机的uuid
+     *     expireTime   //过期时间，例如有效期3小时，这个时间可以自行设定
+     *     decisionType //0表示用户可以正常注册，1表示需要用户通过滑块验证，通过后才能注册，2表示需要用户过一段时间，才能重新注册，3表示这个用户不能注册
      * }
      */
     @PostMapping("/register")
@@ -93,15 +93,15 @@ public class AuthController {
     /**
      * 登出或注销
      * @param requestBody
-     * SessionID
-     * ActionType  //1代表登出，2代表注销
-     * Environment{
-     *     IP
-     *     DeviceID
+     * sessionId
+     * actionType  //1代表登出，2代表注销
+     * environment{
+     *     ip
+     *     deviceId
      * }
      * @return
-     * Code      //0表示登出或注销成功，1表示登出或注销失败
-     * Message   //表示返回的说明，例如退出时，code=0，message=“退出成功”
+     * code      //0表示登出或注销成功，1表示登出或注销失败
+     * message   //表示返回的说明，例如退出时，code=0，message=“退出成功”
      */
     @PostMapping("/logout")
     public CommonResult<Map<String, Object>> logout(@RequestBody Map<String, Object> requestBody) {
